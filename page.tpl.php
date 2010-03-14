09ed60dc56ca49263b5895d5da7b39d677f41e4f
Ming-Ting Yao Wei <mwei@lxde.org>
2010-03-14 20:46:01 +0800

    modified code style, as well as packaging content area

--- page.tpl.php
+++ page.tpl.php
@@ -14,80 +14,84 @@
 
 <body class="<?php print $body_classes; ?>">
 
-	<header>
-		<?php if (!empty($logo)): ?>
-			<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>"></a>
-		<?php endif; ?>
+<header>
+<?php if (!empty($logo)): ?>
+	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>"></a>
+<?php endif; ?>
 
-		<?php if (!empty($site_name)): ?><h1><a href="<?php print $front_page ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a></h1><?php endif; ?>
-		<?php if (!empty($site_slogan)): ?><p><?php print $site_slogan; ?></p><?php endif; ?>
+<?php if (!empty($site_name)): ?><h1><a href="<?php print $front_page ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a></h1><?php endif; ?>
+<?php if (!empty($site_slogan)): ?><p><?php print $site_slogan; ?></p><?php endif; ?>
 
-	</header>
+</header>
 	
-	<nav></nav>
+<nav></nav>
+
+<section class="context">
 
 	<article>
 
 <?php	if ($content_top) { ?>
 
-                <header>
+		<header>
 
-			<?php print $content_top; ?>
-			
+<?php print $content_top; ?>
+				
 		</header>
 
 <?php	} 
-	
-	if ($tabs) { ?>
-	
-		<!-- Tabs for those who have permissions to edit -->
+		
+	if ($tabs) {	/* Tabs for those who have permissions to edit */ ?>
+		
 
 		<nav class="tabs">
 
-			<?php print $tabs; ?>
+<?php print $tabs; ?>
 
 		</nav>
 
 <?php	} ?>
 
-		<?php print $content; ?>
+<?php print $content; ?>
 
-		<?php if ($content_bottom): ?>
+<?php if ($content_bottom): ?>
 
-			<footer>
+		<footer>
 
-				<?php print $content_bottom; ?>
+<?php print $content_bottom; ?>
 
-			</footer>
+		</footer>
 
-		<?php endif; ?>
+<?php endif; ?>
 
 	</article>
 
-		<?php if (!empty($primary_links) || $navigation_bar): ?>
-			<nav>
-				<?php print theme('links', $primary_links, array('id' => 'primary', 'class' => 'links main-menu'));?>
-				<?php if ($navigation_bar) print $navigation_bar; ?>
-			</nav>
-		<?php endif; ?>
+</section>
 
-		<?php if ($sponsors): ?>
-			<aside id="sponsors">
-				<?php print $sponsors; ?>
-			</aside>
-		<?php endif; ?>
+<?php if (!empty($primary_links) || $navigation_bar): ?>
+<nav>
+<?php print theme('links', $primary_links, array('id' => 'primary', 'class' => 'links main-menu'));?>
+<?php if ($navigation_bar) print $navigation_bar; ?>
+</nav>
+<?php endif; ?>
 
-		<?php if ($sidebar): ?>
-			<aside id="sidebar">
-				<?php print $sidebar; ?>
-			</aside>
-		<?php endif; ?>
+<?php if ($sponsors): ?>
+<aside id="sponsors">
+<?php print $sponsors; ?>
+</aside>
+<?php endif; ?>
 
-	<footer>
-	<?php print $footer_message; ?>
-	<?php print $footer_block; ?>
-	</footer>
+<?php if ($sidebar): ?>
+<aside id="sidebar">
+<?php print $sidebar; ?>
+</aside>
+<?php endif; ?>
 
-		<?php print $closure; ?>
-	</body>
+<footer>
+<?php print $footer_message; ?>
+<?php print $footer_block; ?>
+</footer>
+
+<?php print $closure; ?>
+
+</body>
 </html>
